b1b35a0d0c2a14eeb9712da6885c6d28b8df13be
KranePch <teevisit_kn@hotmail.com>
2024-03-03 19:39:09 +0700

    Add: load data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   // 1. create a state variable named `countries` to store the countries as array.
   const [text, setText] = useState("");
+
   // 2. create a state variable named `loading` to store the loading state as boolean.
+  const [loading, setLoading] = useState(false);
 
   // 3. create a useEffect to fetch the countries from the API.
   //    💡 TIP: open "https://restcountries.com/v3.1/all" to see the data in your browser
   //    💡 TIP: use `fetch` API to fetch the data.
+  const [countries, setContries] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const data = await res.json();
+        setContries(data);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [])
+
 
   return (
     <main
@@ -18,45 +38,45 @@ function App() {
     >
       <h1>Country Finder</h1>
       {/* 4. render "loading..." or an icon when the `loading` state is true otherwise, render the input and countries. */}
-      <input
-        placeholder="Type to find..."
-        value={text}
-        onChange={(event) => {
-          setText(event.target.value);
-        }}
-        style={{
-          border: "none",
-          background: "#f5f5f5",
-          padding: "0.75rem 1rem",
-          width: "100%",
-          fontSize: "1rem",
-        }}
-      />
-      <ul style={{ margin: 0, padding: 0, marginTop: "1rem" }}>
-        {/* 5. replace the mockup data with the `countries` from the API
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <input
+            placeholder="Type to find..."
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+            style={{
+              border: "none",
+              background: "#f5f5f5",
+              padding: "0.75rem 1rem",
+              width: "100%",
+              fontSize: "1rem",
+            }}
+          />
+          <ul style={{ margin: 0, padding: 0, marginTop: "1rem" }}>
+            {/* 5. replace the mockup data with the `countries` from the API
                and make sure to use the right properties.
         */}
-        {[
-          { name: "United States", flag: "🇺🇸" },
-          { name: "Brazil", flag: "🇧🇷" },
-          { name: "Thailand", flag: "🇹🇭" },
-          { name: "Japan", flag: "🇯🇵" },
-          { name: "China", flag: "🇨🇳" },
-        ]
-          .filter((country) => country.name.includes(text))
-          .map((country) => (
-            <li
-              key={country.name}
-              style={{
-                display: "flex",
-                borderBottom: "1px solid #e5e5e5",
-                padding: "0.5rem 0",
-              }}
-            >
-              {country.flag} {country.name}
-            </li>
-          ))}
-      </ul>
+            {countries
+              .filter((country) => country.name.official.includes(text))
+              .map((country) => (
+                <li
+                  key={country.name.official}
+                  style={{
+                    display: "flex",
+                    borderBottom: "1px solid #e5e5e5",
+                    padding: "0.5rem 0",
+                  }}
+                >
+                  {country.flag} {country.name.official}
+                </li>
+              ))}
+          </ul>
+        </>
+      )}
     </main>
   );
 }
